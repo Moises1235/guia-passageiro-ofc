824df4d9746d90a28d5de5eff9317ca19ed87123
Moises1235 <127400299+Moises1235@users.noreply.github.com>
2023-10-16 22:19:05 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
 const result = document.querySelector(".result");
+const result2 = document.querySelector(".invisivel");
 
 function descricao() {
     result.classList.toggle('escondidinho')
 
+}
+
+
+
+function formasPagamento(){
+    result2.classList.toggle('escondidinho')
 }
